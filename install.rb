e17c6556f9c7458b14bf82e05b5408818d032b66
Sri Prasanna <k.sriprasanna@gmail.com>
2010-10-22 23:05:14 +0530

    misc

--- install.rb
+++ install.rb
@@ -4,5 +4,5 @@ Dir['*'].each do |file|
   link = file
   link = "." + file unless file == "bin"
   puts "linking #{current_directory}/#{file} --> ~/#{link}"
-  system "ln -s #{current_directory}/#{file} ~/#{link}"
+  system "ln -nfs #{current_directory}/#{file} ~/#{link}"
 end
