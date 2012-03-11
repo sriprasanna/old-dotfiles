975875ec54d5a8c638412f3662f732adda6106a9
Sri Prasanna <k.sriprasanna@gmail.com>
2012-03-11 13:29:27 +0530

    changes

--- install.rb
+++ install.rb
@@ -2,7 +2,7 @@
 current_directory = Dir.pwd
 Dir['*'].each do |file|
   link = file
-  link = "." + file unless file == "bin"
+  link = "." + file unless file == "mybin"
   puts "linking #{current_directory}/#{file} --> ~/#{link}"
   system "ln -nfs #{current_directory}/#{file} ~/#{link}"
 end
